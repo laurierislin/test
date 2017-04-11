802c7605b0a87c767b83cf8c67a558039a029579
laurie <laurie@rislin.com>
2017-04-11 18:00:43 +1000

    adding test web service

---
+++ test/src/test/java/com/rislin/test/MyResourceTest.java
@@ -0,0 +1,13 @@
+package com.rislin.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyResourceTest {
+
+	@Test
+	public void testDummy() {
+		assertTrue(true);
+	}
+}
